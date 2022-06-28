abec2a4a60489fead7bbc08a04e6086eae48ecd9
joseph-birara <josephbirara92@gmail.com>
2022-06-28 22:43:37 +0300

    task 7 solved

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from sqlalchemy import false, true
+
 
 
 def islower(c):
     if c >= 'a' and c <= 'z':
-        return true
+        return 1
     else:
-        return false
+        return 0

ca7e2d346bdfefb4865ee339b6fff555cd807127
joseph-birara <josephbirara92@gmail.com>
2022-06-30 20:37:05 +0300

    make task 2 executable

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import mul, div, add, sub
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

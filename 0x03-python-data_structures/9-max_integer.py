33856407f602efc991788ef2621f2b89bf3181a7
joseph-birara <josephbirara92@gmail.com>
2022-07-03 10:31:44 +0300

    tasks 8-12 solved

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        big = [0]
+        for i in my_list:
+            if i > big:
+                big = i
+    return big

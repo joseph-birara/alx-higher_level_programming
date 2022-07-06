47ca0804581e0fc2850f3fa5d7bdf48f17679b65
joseph-birara <josephbirara92@gmail.com>
2022-07-06 17:35:47 +0300

    bug fixes

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,8 +3,6 @@ from tempfile import tempdir
 
 
 def simple_delete(a_dictionary, key=""):
-    temp = dict()
-    for m in a_dictionary:
-        if m != key:
-            temp[m] = a_dictionary[m]
-    return temp
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] = 2 * a_dictionary[i]
-    return a_dictionary
+    b_dictionary = dict()
+    for i in a_dictionary.keys():
+        b_dictionary[i] = 2 * a_dictionary[i]
+    return b_dictionary

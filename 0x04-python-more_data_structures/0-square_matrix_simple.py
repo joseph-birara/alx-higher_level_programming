374e55ad5c3a52b5aca78e4aba397f77ba6d7a25
joseph-birara <josephbirara92@gmail.com>
2022-07-06 15:06:16 +0300

    readme and task 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    temp = [[x[i]**2 for i in range(len(matrix))] for x in matrix]
+    temp = [[x[i]**2 for i in range(len(x))] for x in matrix]
     return temp
 

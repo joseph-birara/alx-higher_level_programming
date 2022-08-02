8b659c94ba4c2b643cd1148f83ffbb151dac77ef
joseph-birara <josephbirara92@gmail.com>
2022-08-03 00:54:25 +0300

    solutioon added

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+File: 4-from_json_string.py
+Desc: This module deals with the json object.
+Date Created: Aug 2 2022
+"""
+from json import loads as ls
+
+
+def from_json_string(my_str):
+    """
+    This function returns an object (Python data structure)
+    represented by a JSON string
+    """
+    return ls(my_str)

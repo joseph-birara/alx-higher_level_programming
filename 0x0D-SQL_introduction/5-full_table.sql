cffa62c72bc8240907af4c4ec4dd816b1826882d
joseph-birara <josephbirara92@gmail.com>
2022-09-06 23:15:14 +0300

    bug fix

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- show tables from specific database
-
-SHOW COLUMNS FROM `first_table`;
+-- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server 
+SHOW CREATE TABLE first_table

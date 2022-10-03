f0ad24c6f165548d28224e6c5ebe69e9084a4a4f
joseph-birara <josephbirara92@gmail.com>
2022-10-03 18:19:30 +0300

    bug fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
+for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
